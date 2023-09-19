75575619442aeeda21d10e1555f253d4a187367f
Anthony Alaribe <anthonyalaribe@gmail.com>
2023-09-19 11:28:09 +0200

    new release

--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name="apitoolkit_flask",
     version="0.1",
     packages=find_packages(),
     description='A Flask SDK for Apitoolkit integration',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author_email='hello@apitoolkit.io',
     author='APIToolkit',
     install_requires=[
