075c2974686f3f071937c41a90be12bb4c2cb1eb
dawkaka <yousiph77@gmail.com>
2023-11-09 13:53:46 +0000

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="apitoolkit-flask",
-    version="0.2.0",
+    version="0.2.2",
     packages=find_packages(),
     description='A Flask SDK for Apitoolkit integration',
     long_description=long_description,
